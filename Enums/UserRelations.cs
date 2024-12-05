deee93cd878066a83bb33e5895cbd15d4969b01a
Hector <hector@peopleofthe.cloud>
2024-12-05 13:37:45 +0100

    DTO y demás de UserRelations

---
+++ Enums/UserRelations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gametopia.Contracts.Enums
+{
+    public class UserRelations
+    {
+        public enum UserRelationType
+        {
+            None = 0,
+            Blocked = 1,
+            Followed = 2,
+            Reported = 3
+        }
+
+        public enum ReportReason
+        {
+            Spam = 1,
+            Harassment = 2,
+            HateSpeech = 3,
+            InappropriateContent = 4,
+            Other = 5
+        }
+    }
+}

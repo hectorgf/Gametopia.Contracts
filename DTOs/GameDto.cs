19b1e4d3bbec600f3d3d4111a573af1a818f89eb
Hector <hector@peopleofthe.cloud>
2024-12-05 12:28:36 +0100

    Cambio de estructura de carpetas

--- DTOs/GameDto.cs
+++ DTOs/GameDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gametopia.Contracts.SteamIntegration
+namespace Gametopia.Contracts.DTOs
 {
     public class GameDto
     {

930d4768ff437b49ba83794468e773a2c21e5359
Hector <hector@peopleofthe.cloud>
2024-12-10 13:10:55 +0100

    ProfileUpdateRequest

---
+++ WebApi/GametopiaUser/ProfileUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gametopia.Contracts.WebApi.GametopiaUser
+{
+    public class ProfileUpdateRequest
+    {
+        public string SteamUser { get; set; }
+        public string Nick { get; set; }
+    }
+
+}

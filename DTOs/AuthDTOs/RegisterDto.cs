781c71f5e9212002202a0dd05268e70a0d9e4491
Hector <hector@peopleofthe.cloud>
2025-07-01 09:20:10 +0200

    Corrección de DTOs

---
+++ DTOs/AuthDTOs/RegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gametopia.Contracts.DTOs.AuthDTOs
+{
+    public class RegisterRequestDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Nick { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string SecondName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}

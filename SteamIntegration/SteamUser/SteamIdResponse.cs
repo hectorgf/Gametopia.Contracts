9630d13cf80b67c9cb572909cb3e0a365a1d319f
Héctor <hectorgf.89@gmail.com>
2025-08-21 23:02:45 +0200

    Añadido nombre del json

--- SteamIntegration/SteamUser/SteamIdResponse.cs
+++ SteamIntegration/SteamUser/SteamIdResponse.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Gametopia.Contracts.SteamIntegration.SteamUser
 {
     public class SteamIdResponse
     {
+        [JsonPropertyName("steamid")]
         public string SteamId { get; set; } = string.Empty;
+        [JsonPropertyName("success")]
         public bool Success { get; set; } = false;
     }
 }

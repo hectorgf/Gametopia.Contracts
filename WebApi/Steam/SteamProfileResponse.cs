a4ba0b9103efa3cba380ae0e5164e1757bf3f97f
Héctor <hectorgf.89@gmail.com>
2025-10-01 12:06:01 +0200

    Añadir campos de relación

--- WebApi/Steam/SteamProfileResponse.cs
+++ WebApi/Steam/SteamProfileResponse.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Gametopia.Contracts.WebApi.Steam
 {
     public class SteamProfileResponse
     {
+        [JsonPropertyName("steamid")]
         public string SteamId { get; set; }
+        [JsonPropertyName("avatar")]
         public string AvatarUrl { get; set; }
+        [JsonPropertyName("profileurl")]
         public string ProfileUrl { get; set; }
+        [JsonPropertyName("personaname")]
         public string DisplayName { get; set; }
         public bool IsPrivate { get; set; }
+        [JsonPropertyName("lastlogoff")]
         public DateTime LastUpdated { get; set; }
         public List<string> GameIds { get; set; } = new List<string>();
         public List<string> GameNames { get; set; } = new List<string>();

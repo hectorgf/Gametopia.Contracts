4b79a9208949a332a4bebb83bdf1d59a3ebc58bb
Héctor <hectorgf.89@gmail.com>
2025-10-04 12:48:07 +0200

    SteamProfileResponse

---
+++ SteamIntegration/SteamUser/SteamProfileResponse.cs
@@ -0,0 +1,20 @@
+﻿using Gametopia.Contracts.Utils.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Gametopia.Contracts.SteamIntegration.SteamUser
+{
+    public class SteamProfileResponse
+    {
+        public string SteamId { get; set; }
+        public string AvatarUrl { get; set; }
+        public string ProfileUrl { get; set; }
+        public string DisplayName { get; set; }
+        public int IsPrivate { get; set; }
+        public DateTime LastLogOff { get; set; }
+    }
+}

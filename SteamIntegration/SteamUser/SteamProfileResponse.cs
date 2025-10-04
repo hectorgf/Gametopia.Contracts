09d59b0d7838ec400fbcb8b3826c322ea1bb5e44
Héctor <hectorgf.89@gmail.com>
2025-10-04 12:14:50 +0200

    Añadir conversor de Unix timestamp a DateTime

--- SteamIntegration/SteamUser/SteamProfileResponse.cs
+++ SteamIntegration/SteamUser/SteamProfileResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using Gametopia.Contracts.Utils.Converters;
+using System.Text.Json.Serialization;
 
 namespace Gametopia.Contracts.SteamIntegration.SteamUser
 {
@@ -6,17 +7,28 @@ namespace Gametopia.Contracts.SteamIntegration.SteamUser
     {
         [JsonPropertyName("steamid")]
         public string SteamId { get; set; }
+        
         [JsonPropertyName("avatar")]
         public string AvatarUrl { get; set; }
+        
         [JsonPropertyName("profileurl")]
         public string ProfileUrl { get; set; }
+        
         [JsonPropertyName("personaname")]
         public string DisplayName { get; set; }
+        
         public bool IsPrivate { get; set; }
-        public DateTime LastUpdated { get; set; }
+        
+        [JsonPropertyName("lastlogoff")]
+        [JsonConverter(typeof(UnixTimestampToDateTimeConverter))]
+        public DateTime LastLogOff { get; set; }
+
         public List<string> GameIds { get; set; } = new List<string>();
+
         public List<string> GameNames { get; set; } = new List<string>();
+
         public int TotalGamesCount { get; set; }
+
         public int TotalPlaytimeMinutes { get; set; }
     }
 }
---
+++ Utils/Converters/UnixTimestampToDateTimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Gametopia.Contracts.Utils.Converters
+{
+    public class UnixTimestampToDateTimeConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Number && reader.TryGetInt64(out long seconds))
+            {
+                return DateTimeOffset.FromUnixTimeSeconds(seconds).UtcDateTime;
+            }
+
+            throw new JsonException("Invalid Unix timestamp.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            long unixTime = ((DateTimeOffset)value).ToUnixTimeSeconds();
+            writer.WriteNumberValue(unixTime);
+        }
+    }
+}
